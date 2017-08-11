eb65b5076225c0a448655e9cfab6df2f7670b1fb
jiangshshui <30767399+jiangshshui@users.noreply.github.com>
2017-08-11 14:37:32 +0800

    firstNumpyDemo

--- NumpyDemo/FirstNumpyDemo.py
+++ NumpyDemo/FirstNumpyDemo.py
@@ -1,2 +1,19 @@
 from numpy import *
-print(random.rand(4,4))
+print(random.rand(4,4)) #这产生的是数组,numpy 的数组和矩阵是不同的
+
+randMat=mat(random.rand(4,4,))
+print("--mat--")
+print(randMat)
+print("--mat's inverse--")
+invRandMat=randMat.I
+print(invRandMat)
+
+print("--invRandMat*randMat--")
+myEye=invRandMat*randMat
+print(myEye)
+print("------误差------")
+print(myEye-eye(4))
+
+print(eye(4))
+
+
